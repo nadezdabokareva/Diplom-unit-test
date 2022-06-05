bed2f8645f4813fb224dde4264d48a9d697206c6
Надежда Сергеевна Бокарева <87896632+nadezdabokareva@users.noreply.github.com>
2022-06-05 20:21:25 +0300

    initial commit

---
+++ src/main/java/praktikum/Bun.java
@@ -0,0 +1,36 @@
+package praktikum;
+
+/**
+ * Модель булочки для бургера.
+ * Булочке можно дать название и назначить цену.
+ */
+public class Bun {
+
+    public String name;
+    public float price;
+
+    public Bun(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bun)) return false;
+
+        Bun bun = (Bun) o;
+
+        if (Float.compare(bun.getPrice(), getPrice()) != 0) return false;
+        return getName().equals(bun.getName());
+    }
+
+}
---
+++ src/main/java/praktikum/Ingredient.java
@@ -0,0 +1,44 @@
+package praktikum;
+
+/**
+ * Модель ингредиента.
+ * Ингредиент: начинка или соус.
+ * У ингредиента есть тип (начинка или соус), название и цена.
+ */
+public class Ingredient {
+
+    public IngredientType type;
+    public String name;
+    public float price;
+
+    public Ingredient(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public IngredientType getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ingredient)) return false;
+
+        Ingredient that = (Ingredient) o;
+
+        if (Float.compare(that.getPrice(), getPrice()) != 0) return false;
+        if (getType() != that.getType()) return false;
+        return getName().equals(that.getName());
+    }
+
+}
